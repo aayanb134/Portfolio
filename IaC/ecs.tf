f22dd4cbf28152067be5102f7cff48ad4cda0d4a
aayanb134 <aayanb134@gmail.com>
2024-11-05 16:24:18 +0000

    added IAM file

--- IaC/ecs.tf
+++ IaC/ecs.tf
@@ -22,30 +22,6 @@ resource "aws_ecs_task_definition" "service" {
   depends_on = [aws_iam_role.ecs_execution_role]
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  depends_on = [aws_iam_role.ecs_execution_role]
-}
-
-
 resource "aws_ecs_service" "main" {
   name            = "portfolio-service"
   cluster         = aws_ecs_cluster.main.id
