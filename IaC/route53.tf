98b0174fef1daa477bb3f8c558da1ae9cd7946c3
aayanb134 <aayanb134@gmail.com>
2024-11-06 17:15:51 +0000

    more troubleshooting

--- IaC/route53.tf
+++ IaC/route53.tf
@@ -27,3 +27,11 @@ module "route53" {
 resource "aws_route53_zone" "primary" {
   name = "aayan-resume.com"
 }
+
+# resource "aws_route53_record" "www" {
+#   name    = "www.aayan-resume.com"
+#   zone_id = aws_route53_zone.primary.id
+#   type    = "CNAME"
+#   ttl     = 300
+#   records = [aws_lb.main.dns_name]
+# }
